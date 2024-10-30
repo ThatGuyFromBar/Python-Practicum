1dfb65b640e1517a33e0b44b44bd9506ba9f4ddb
ThatGuyFromBar <122010936+ThatGuyFromBar@users.noreply.github.com>
2024-10-30 15:09:04 +0300

    Update task5.py
    Optimised task

--- ExamTasks/task5.py
+++ ExamTasks/task5.py
@@ -1,19 +1,11 @@
 def main():
     n = int(input())
-    msr = 10000000
-    for i in range(n):
-        s = ""
-        sr = 0
-        k = 0
-        while s != "end":
-            s = input()
-            if s != "end":
-                s = int(s)
-                k += 1
-                sr += s
-        sr = sr / k
-        if sr < msr:
-            msr = sr
+    msr = 1000000000
+    for i in range(int(input())):
+        ns = []
+        while (s := input()) != "end":
+            ns.append(int(s))
+        msr = min(msr, sum(ns) / len(ns))
     print("{:.2f}".format(msr))
 
 
