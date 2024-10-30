30c722ad451b18cad735d62ad18e51f09c78e1ea
ThatGuyFromBar <122010936+ThatGuyFromBar@users.noreply.github.com>
2024-10-30 15:08:40 +0300

    Update task4.py
    Optimised task

--- ExamTasks/task4.py
+++ ExamTasks/task4.py
@@ -1,14 +1,6 @@
 def main():
-    n = int(input())
-    m = int(input())
-    xl = int(input())
-    xm = xl - 1000000
-    for i in range(n - 1):
-        x = int(input())
-        if abs(x - xl) < m and x > xm:
-            xm = x
-        xl = x
-    print(xm)
+    ns = [int(input()) for i in range(int(input()))]
+    print(max([ns[i] for i in range(1, len(ns)) if ns[i] - ns[i - 1] < ns[0]]))
 
 
 if __name__ == "__main__":
